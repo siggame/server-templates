c157f91ad1998d6caa6387dd77f6f345b6a04535
Steven Wallace <snwallace@gmail.com>
2013-07-28 13:31:05 -0500

    Cleaned up game_state_attributes a bit.

--- templates/${name}/objects.py
+++ templates/${name}/objects.py
@@ -3,5 +3,8 @@ from .game import Game
 
 % for model in models:
 class ${model.name}(Game.Object):
-    game_state_attributes = set([${', '.join([repr(i.name) for i in model.data])}])
+    game_state_attributes = ${repr(
+        sorted([i.name for i in model.data])
+        )}
+
 % endfor
