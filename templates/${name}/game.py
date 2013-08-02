c3826f9d2a69faea86175c46841d9d21866bcc10
Steven Wallace <snwallace@gmail.com>
2013-08-01 21:46:24 -0500

    Added various function stubs.

--- templates/${name}/game.py
+++ templates/${name}/game.py
@@ -2,4 +2,39 @@ import game_objects
 import objects
 
 class Game(game_objects.Game):
-    pass
+    def before_start(self):
+        #TODO: Initialize the game
+
+        #Create any objects that should exist at the start (Tiles, for example)
+        #Initialize any global values
+        #At this point Player objects exist
+        #(But any game-specific values will be uninitialized)
+        pass
+
+    def before_turn(self):
+        #TODO: Initialize the turn
+        #turn_number and current_player will be valued for the coming turn
+
+        #Common operations include:
+        #Setting current player's units to ready to move/attack
+        #Start of turn income
+        #Creating units whose construction began previously
+        pass
+
+    def after_turn(self):
+        #TODO: Clean up any values at the end of a turn
+        #turn_number and current_player will be valued for the past turn
+        #This is called before check_winner, so this is a good place for any
+        #Score calculation
+
+        #Common operations include:
+        #Setting all units to no moves_left/attacks_left
+        #Any end of turn costs/damage
+        pass
+
+    def check_winner(self):
+        #TODO: Calculate if anyone has won and return the winner and reason
+        if turn_number >= 500:
+            return self.objects.players[0], 'won due to tie'
+        else:
+            return None, 'the battle continues'
