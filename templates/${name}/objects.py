ad4a8445587df62ab0cbb39349d6b8a3a9cbfb7d
Steven Wallace <snwallace@gmail.com>
2013-09-21 17:17:17 -0500

    Added init stub to object templates.

--- templates/${name}/objects.py
+++ templates/${name}/objects.py
@@ -15,6 +15,11 @@ class ${model.name}(Game.Object):
         {i.name: i.through for i in model.remotes}
         )}
 
+    def __init__(self, game, **kwargs):
+        Game.Object.__init__(self, game, **kwargs)
+        #TODO: Fill in any work that needs to be done when an object is made
+        #Common example would be setting the unit's health to maximum
+
     def before_turn(self):
         #TODO: Fill in start of turn values
         #Common example would be giving units moves before their turn
